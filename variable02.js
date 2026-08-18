96b36da78c84580fdd21f24fe6de97ddf4d090c2
Santhosh M <santhoshrvs26@gmail.com>
2026-08-18 11:16:15 +0530

    Deleted unused JS files).

--- variable02.js
+++
@@ -1,8 +0,0 @@
-// Array containing numbers 1 through 10
-let numArray = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-
-// Declare a variable using let
-let elementAtIndexFive = numArray[5];
-
-// Print the value to the console
-console.log("Element at index 5:", elementAtIndexFive);
